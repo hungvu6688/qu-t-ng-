f19846c90fddcde2f9979bcfc49d18401b6d37a7
hungvu6688 <100954062+hungvu6688@users.noreply.github.com>
2022-03-05 01:32:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc M?ng 8/3';
+const titleCard = 'T?ng ngu?i ?y';
+const contentCard = 'Chúc honey c?a anh 8/3 tràn ng?p ni?m vui và nh?ng n? cu?i. Mong di?u d?p nh?t s? d?n v?i em trong hôm nay và c? nh?ng ngày sau';
